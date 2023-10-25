8075507b7a72884014a1ab3128afd5c1aafa8388
YeYint009 <blackskym96@gmail.com>
2023-10-26 03:24:26 +0900

    main

---
+++ src/js/event.js
@@ -0,0 +1,33 @@
+// const fileInput = document.querySelector("#fileInput");
+// fileInput.addEventListener("change",() => {
+//   console.log(fileInput.files);
+// })
+
+// const testForm = document.querySelector("#testForm");
+// const mmsLink = document.querySelector("#mmsLink");
+// console.log(testForm);
+
+// testForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log("U submit");
+
+
+   
+// });
+
+// mmsLink.addEventListener("click", (event) => {
+//   console.log(event);
+//   event.preventDefault();
+//   console.log("U Click Link");
+// });
+// window.addEventListener("scroll",() => {
+//   console.log("scrolled");
+//   console.log(window.scrollY);
+// })
+
+
+document.addEventListener("mousemove",(event) => {
+  console.clear();
+  console.log(event);
+})
